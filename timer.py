bc5f9a55adde78ffa5da612f49efe39ede6169f2
internetperson12 <49924777+internetperson12@users.noreply.github.com>
2019-04-23 19:49:43 -0400

    Change convert & add comment
    Added comments & changed convert.

--- timer.py
+++ timer.py
@@ -1,27 +1,27 @@
 import time
 class timer:
-	def __init__(self):
+	def __init__(self): # Init function. Runs when you create a timer
 		self.ms = 0
 		self.stx = None
 		self.running = False
 		self.endtime = 0.00
 
-	def start(self):
+	def start(self): # Start a timer. | Returns nothing
 		self.running = True
 		self.stx = round(time.time()*1000.00)
 
 
-	def end(self):
+	def end(self): # End running timer. | Returns end time (Seconds)
 		self.running = False
 		self.endtime = round(time.time() * 1000.00) - self.stx
 
-	def convert(self):
+	"""def convert(self):
 		if not self.endtime:
 			return None
 		self.seconds = self.endtime / 1000
-		return self.seconds
+		return self.seconds"""
 
-	def gettime(self):
+	def gettime(self): # Grabs time of timer. | Returns seconds of timer
 		if not self.stx:
 			return None
 		if self.endtime:
@@ -29,3 +29,22 @@ class timer:
 		self.current = round(time.time() * 1000.00) - self.stx 
 		self.seconds = self.current / 1000
 		return self.seconds
+
+	@staticmethod # Method is not using object so we dont need it
+	def convert(time): # Takes time in seconds and converts to minutes. | Returns: Time in Minutes 
+
+		"""Pair with <timer>.gettime() for best result. 
+
+		EX:
+
+		Clock = timer()
+
+		print( Clock.convert(Clock.gettime()) ) // 0.00000000
+
+		time.sleep(60)
+
+		print( Clock.convert(Clock.gettime()) ) // 60.0000000"""
+		if not time > 0:
+			return None
+		minutes = time / 60
+		return minutes
